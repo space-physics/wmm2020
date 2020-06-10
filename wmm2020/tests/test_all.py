018dae41248b2cc8cfa38b62aa9283ec419599ba
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-10 03:57:27 -0400

    update test data

--- wmm2020/tests/test_all.py
+++ wmm2020/tests/test_all.py
@@ -8,13 +8,13 @@ import wmm2020 as wmm
 def test_wmm():
     mag = wmm.wmm(65, 85, alt_km=0, yeardec=2012.52868852459)
 
-    assert mag.north.item() == approx(9215.692665)
-    assert mag.east.item() == approx(2516.0058789)
-    assert mag.down.item() == approx(59708.529371)
-    assert mag.total.item() == approx(60467.906831)
+    assert mag.north.item() == approx(9216.095937998032)
+    assert mag.east.item() == approx(2585.6313552791953)
+    assert mag.down.item() == approx(59578.81210945489)
+    assert mag.total.item() == approx(60342.82696574229)
 
-    assert mag.incl.item() == approx(80.910090)
-    assert mag.decl.item() == approx(15.27036)
+    assert mag.incl.item() == approx(80.87285397874935)
+    assert mag.decl.item() == approx(15.67178464900435)
 
 
 if __name__ == "__main__":
