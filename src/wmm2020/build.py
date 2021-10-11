db7be2cd4a84dc55e66e3bbf1523a62f25ec8ad3
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-10 23:31:32 -0400

    simplify build

--- src/wmm2020/build.py
+++ src/wmm2020/build.py
@@ -4,23 +4,32 @@ A generic, clean way to build C/C++/Fortran code "build on run"
 Michael Hirsch, Ph.D.
 https://www.scivision.dev
 """
+
 import shutil
 from pathlib import Path
 import importlib.resources
 import subprocess
 import sys
+import os
 
 
 def build():
     """
     attempt to build using CMake
     """
-    exe = shutil.which("ctest")
+
+    exe = shutil.which("cmake")
     if not exe:
         raise FileNotFoundError("CMake not available")
 
-    with importlib.resources.path(__package__, "setup.cmake") as file:
-        subprocess.check_call([exe, "-S", str(file), "-VV"])
+    with importlib.resources.path(__package__, "CMakeLists.txt") as f:
+        s = f.parent
+        b = s / "build"
+        g = []
+        if sys.platform == "win32" and not os.environ.get("CMAKE_GENERATOR"):
+            g = ["-G", "MinGW Makefiles"]
+        subprocess.check_call([exe, f"-S{s}", f"-B{b}"] + g)
+        subprocess.check_call([exe, "--build", str(b), "--parallel"])
 
 
 def get_libpath(bin_dir: Path, stem: str) -> Path:
@@ -30,5 +39,7 @@ def get_libpath(bin_dir: Path, stem: str) -> Path:
         dllfn = bin_dir / ("lib" + stem + ".so")
     elif sys.platform == "darwin":
         dllfn = bin_dir / ("lib" + stem + ".dylib")
+    else:
+        raise ValueError(f"Unsupported platform: {sys.platform}")
 
     return dllfn
